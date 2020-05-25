9598f453acc86e323bb6ec7eab36ee85e9c3c28c
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-05-25 14:49:59 -0400

    2: Add function countBits

--- codewars-js.js
+++ codewars-js.js
@@ -4,3 +4,15 @@
    You can guarantee that input is non-negative.
    Example: The binary representation of 1234 is 10011010010, 
    so the function should return 5 in this case*/
+
+var countBits = function(n) {
+	// Convert number to binary string
+	var stringBinary, bitsNum
+	stringBinary = (n>>>0).toString(2);
+
+	// Convert string into Array, filter by '1', take a length of the new array
+	bitsNum = stringBinary.split('').filter(item => {return (item === '1')}).length; 
+	  
+	// Return converted string into integer number
+	return parseInt(bitsNum);
+};
