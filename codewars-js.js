555e30fb8fa5b313bcd675ff099096b3a766a800
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-05-25 18:04:46 -0400

    3: Add function squareDigits

--- codewars-js.js
+++ codewars-js.js
@@ -16,3 +16,24 @@ var countBits = function(n) {
 	// Return converted string into integer number
 	return parseInt(bitsNum);
 };
+
+/* square every digit of a number.
+   For example, if we run 9119 through the function, 
+   811181 will come out, because 9^2 is 81 and 1^2 is 1.
+*/
+
+function squareDigits(num){
+    var reminder,squareArray = [];
+    
+   while(num > 0) {
+    // Get reminder
+    reminder = num % 10;
+    // Get new number (num - reminer)
+    num = Math.floor(num/10);
+    // Power reminder
+    square = Math.pow(reminder, 2);
+    squareArray.push(square);
+   }
+   // reverse array, join, convert to number
+   return parseInt(squareArray.reverse().join(''));
+}
