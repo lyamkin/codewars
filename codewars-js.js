5a01a2530a6e1add279db06976787bb4e86bf086
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-05-26 16:25:44 -0400

    4: Add function isValidWalk

--- codewars-js.js
+++ codewars-js.js
@@ -37,3 +37,42 @@ function squareDigits(num){
    // reverse array, join, convert to number
    return parseInt(squareArray.reverse().join(''));
 }
+
+/* You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+   You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go
+   for a short walk. The city provides its citizens with a Walk Generating App on their phones --
+   everytime you press the button it sends you an array of one-letter strings representing directions
+   to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction and you 
+   know it takes you one minute to traverse one city block, so create a function that will return
+   true if the walk the app gives you will take you exactly ten minutes (you don't want to be early 
+   or late!) and will, of course, return you to your starting point. Return false otherwise.
+   Note: you will always receive a valid array containing a random assortment of direction letters 
+   ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's 
+   standing still!).
+*/
+function isValidWalk(walk) {
+   var sidesArr, sidesObj, numSteps;
+   numSteps = walk.length;
+   sidesArr = ['n','s','w','e'];
+   sidesObj = {
+      n: [],
+      s: [],
+      w: [],
+      e: []
+   };
+   if (numSteps == 10) {
+      // Check how many steps n,s,w,e
+      sidesArr.forEach(side => {
+         sidesObj[side] = walk.filter(stepsSide => {return stepsSide === side});
+      });
+
+      // If n != s and w != e The path is not valid
+      if(sidesObj.n.length === sidesObj.s.length && sidesObj.w.length === sidesObj.e.length) {
+         return true;
+      } else {
+         return false;
+      }
+   } else {
+      return false;
+   }
+ }
