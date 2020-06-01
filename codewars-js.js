fb15e490f41cb73b2b8717cbb4983e861c2069fd
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-05-31 21:56:00 -0400

    13: Add function duplicateEncode codewars-js.js

--- codewars-js.js
+++ codewars-js.js
@@ -280,3 +280,51 @@ function isPangram(string){
  var string = "This is not a pangram";
 console.log(isPangram(string));
 
+
+/*
+The goal of this exercise is to convert a string to a new string where each character
+in the new string is "(" if that character appears only once in the original string, or ")" 
+if that character appears more than once in the original string. Ignore capitalization when 
+determining if a character is a duplicate.
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. 
+If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
+
+// My solution
+function duplicateEncode(word){
+   let obj = {};
+   let arr = word.toLowerCase().split('');
+   let code='';
+
+   arr.forEach(el => {
+      obj[el] ? obj[el]++ : obj[el] = 1;
+   });
+
+   arr.forEach(el => {
+      obj[el]>1 ? code+=')' : code+='('
+   });
+
+   return code;
+
+}
+
+// Best practice
+
+function duplicateEncode(word){
+   return word
+     .toLowerCase()
+     .split('')
+     .map( function (a, i, w) {
+       return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+     })
+     .join('');
+ }
+
+ console.log(duplicateEncode("Succccess"));
+
