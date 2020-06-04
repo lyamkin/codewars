3c9e2d2e460332a7dd6ad4cef0a2ef07a9fcd5bb
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-06-04 14:40:24 -0400

    16: Add function moveZeros -> codewars-js.js

--- codewars-js.js
+++ codewars-js.js
@@ -397,6 +397,23 @@ function validParentheses(parens){
    return counter == 0;
 }
 
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+var moveZeros = function (arr) {
+   let notZeroArr = [], zeroArr = [];
+   arr.forEach(element => {
+      element === 0 ? zeroArr.push(element) : notZeroArr.push(element);
+   });
+   return notZeroArr.concat(zeroArr);
+
+ }
+
+ console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
+
 
 
 
